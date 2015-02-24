7b4717340d8d4d6bd40af838a42e6a3a2601191b
AndreasHassing <andreas@famhassing.dk>
2015-02-24 01:43:10 +0100

    add sample application to test gradle build

--- src/main/java/dk/itu/osmviewer/Main.java
+++ src/main/java/dk/itu/osmviewer/Main.java
@@ -1,12 +1,20 @@
 package dk.itu.osmviewer;
 
-/**
- * Combines the model, view and controller and runs it all neatly.
- */
-public final class Main {
+// JavaFX utils
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
 
-  /** Hidden! */
-  private Main() { }
+// FXML utils
+import javafx.fxml.FXMLLoader;
+
+import dk.itu.osmviewer.controller.ErrorController;
+
+/**
+ * Main class.
+ */
+public final class Main extends Application {
 
   /**
    * Returns the integer it gets as input. It is indeed,
@@ -23,7 +31,40 @@ public final class Main {
    * @param args a string array of arguments passed to the program.
    */
   public static void main(final String[] args) {
-    System.out.println("The program runs. Woop woop!");
+    // Launch JavaFX Application
+    Main.launch();
+  }
+
+  /**
+   * Start JavaFX and move into the main controller.
+   * @param primaryStage The primary stage of the application.
+   */
+  @Override
+  public void start(final Stage primaryStage) {
+    Thread.currentThread().setUncaughtExceptionHandler(
+      // Recklessly stolen from Kasper Isager (for displaying purposes)
+      ErrorController::crash
+    );
+
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader();
+
+      Parent root = fxmlLoader.load(
+        this.getClass().getResource("view/Application.fxml")
+      );
+
+      Scene scene = new Scene(root);
+
+      primaryStage.setTitle("C-Vitamin Danmarkskort");
+      primaryStage.setWidth(800);
+      primaryStage.setHeight(600);
+      primaryStage.setScene(scene);
+      primaryStage.setResizable(false);
+      primaryStage.show();
+    } catch (Throwable t) {
+      // Recklessly stolen from Kasper Isager (for displaying purposes)
+      ErrorController.crash(Thread.currentThread(), t);
+    }
   }
 }
 
